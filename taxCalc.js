bc4f1a040d9688a008078bd48227561d9ad4c485
Richard Lo <richardjosonlo@gmail.com>
2018-07-04 13:24:07 +0100

    added function for calculateSingleTaxBracket and converted dollars into cents for computation

--- taxCalc.js
+++ taxCalc.js
@@ -1,23 +1,41 @@
 
-module.exports.calculateTax = function(taxableIncome) {
-  return 2383.75;
-};
-
-
-module.exports.whichBracket = function(taxRates, taxableIncome) {
+module.exports.calculateTax = function(taxBracket, taxableIncome) {
+  let tax;
   let bracket;
   let margTaxRate;
   let minIncome;
   let maxIncome;
 
-  for (let i = 0; i < taxRates.length; i++) {
-    margTaxRate = taxRates[i];
-    minIncome = margTaxRate[1];
-    maxIncome = margTaxRate[2];
-    if (taxableIncome >= minIncome && taxableIncome < maxIncome) {
+  bracket = self.whichBracket(taxBracket, taxableIncome);
+
+  return tax;
+};
+
+module.exports.whichBracket = function(taxBracket, taxableIncome) {
+  let bracket;
+  let margTaxBracket;
+  let bracketMin;
+  let bracketMax;
+
+  for (let i = 0; i < taxBracket.length; i++) {
+    margTaxBracket = taxBracket[i];
+    bracketMin = margTaxBracket[1];
+    bracketMax = margTaxBracket[2];
+    if (taxableIncome > bracketMin && taxableIncome <= bracketMax) {
       bracket = i;
     }
   }
 
   return bracket;
+};
+
+module.exports.calculateSingleBracket = function(taxBracket, income) {
+  const bracketMin = taxBracket[1];
+  const taxRate = taxBracket[0];
+  let tax;
+
+  let taxableIncome = income - bracketMin;
+  tax = taxableIncome * taxRate;
+
+  return Math.round(tax);
 };
--- test/taxCalc-spec.js
+++ test/taxCalc-spec.js
@@ -5,51 +5,93 @@ const expect = Chai.expect;
 
 const taxCalc = require('../taxCalc.js');
 
-const taxRates2017 = [
-  [.10, 0, 9326],
-  [.15, 9326, 37951],
-  [.25, 37951, 91901],
-  [.28, 91901, 191651],
-  [.33, 191651, 416701],
-  [.35, 416701, 418401],
-  [.396, 418401, 418401],
+const taxBracket2017 = [
+  [.10, 0, 9325],
+  [.15, 9326, 37950],
+  [.25, 37951, 91900],
+  [.28, 91901, 191650],
+  [.33, 191651, 416700],
+  [.35, 416701, 418400],
+  [.396, 418401, 1000000000000],
+];
+
+// Normalizes bracket for usage in this program.
+// Minus 1 from all minimum threshholds greater than 0.
+// Convert dollars into cents by multiplying all threshholds by 100.
+const normalizedTaxBracket2017 = [
+  [.10, 0, 932500],
+  [.15, 932500, 3795000],
+  [.25, 3795000, 9190000],
+  [.28, 9190000, 19165000],
+  [.33, 19165000, 41670000],
+  [.35, 41670000, 41840000],
+  [.396, 41840000, 1000000000000],
 ];
 
 describe('#Calculate effective tax rate', function() {
-  it('should exist', function() {
+  it('taxCalc.js file should exist', function() {
       expect(taxCalc).to.not.be.undefined;
   });
 
   it('should calculate effective tax rate', function() {
       let taxAmount;
-
+/*
       // Check tax on $19,000. Should be $2,383.75
-      taxAmount = taxCalc.calculateTax(19000);
-      expect(taxAmount).to.equal(2383.75);
+      //taxAmount = taxCalc.calculateTax(taxRates2017, 19000);
+      expect(taxCalc.calculateTax(taxRates2017, 19000)).to.equal(2383.75);
 
-      // // Check tax on $80,000. Should be $15,738.75
-      // taxAmount = taxCalc.calculateTax(80000);
-      // expect(taxAmount).to.equal(15738.75);
+      // Check tax on $80,000. Should be $15,738.75
+      //taxAmount = taxCalc.calculateTax(taxRates2017, 80000);
+      expect(taxCalc.calculateTax(taxRates2017, 80000)).to.equal(15738.75);
+
+*/
   });
 });
 
 describe('#Which tax bracket', function() {
   // Function should exist
-  it('should exist', function() {
+  it('whichBracket function should exist', function() {
     expect(taxCalc.whichBracket).to.not.be.undefined;
   });
 
+  // Check tax bracket for $80,000. Should be 2.
   it('should return the tax bracket based on income ($80,000)', function() {
-    expect( taxCalc.whichBracket(taxRates2017, 80000) ).to.equal(2);
+    expect(taxCalc.whichBracket(normalizedTaxBracket2017, 8000000)).to.equal(2);
   });
 
   // Check tax bracket for $19,000. Should be 1.
   it('should return the tax bracket based on income ($19,000)', function() {
-    expect( taxCalc.whichBracket(taxRates2017, 19000) ).to.equal(1);
+    expect(taxCalc.whichBracket(normalizedTaxBracket2017, 1900000)).to.equal(1);
   });
 
   // Check tax bracket for $191,651. Should be 4.
   it('should return the tax bracket based on income ($191,651)', function() {
-    expect( taxCalc.whichBracket(taxRates2017, 191651) ).to.equal(4);
+    expect(taxCalc.whichBracket(normalizedTaxBracket2017, 19165100)).to.equal(4);
+  });
+});
+
+describe('#Calculate single tax bracket', function() {
+  it('calculateSingleBracket function should exist', function() {
+    expect(taxCalc.calculateSingleBracket).to.not.be.undefined;
+  });
+
+  it('should retun tax owed for bracket ($91,900.00)', function() {
+    expect( taxCalc.calculateSingleBracket([.25, 3795000, 9190000],
+      9190000)).to.be.equal(1348750);
+  });
+
+  it('should retun tax owed for bracket ($115,000.00)', function() {
+    expect( taxCalc.calculateSingleBracket([.28, 9190000, 19165000],
+      11500000)).to.be.equal(646800);
+  });
+
+  it('should retun tax owed for bracket ($107,000.00)', function() {
+    expect( taxCalc.calculateSingleBracket([.28, 9190000, 19165000],
+      10700000)).to.be.equal(422800);
+  });
+
+  it('should retun tax owed for bracket ($106,187.61)', function() {
+    expect( taxCalc.calculateSingleBracket([.28, 9190000, 19165000],
+      10618761)).to.be.equal(400053);
   });
 });
