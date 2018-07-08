ebe57d201badebd9c348c70a54e94d51a0794acd
Richard Lo <richardjosonlo@gmail.com>
2018-07-08 14:48:18 +0100

    added 'lower' function

---
+++ routes/convert-case-server.js
@@ -0,0 +1,3 @@
+module.exports.lower = function(text) {
+  return text.toLowerCase();
+};
--- test/convert-case-server-spec.js
+++ test/convert-case-server-spec.js
@@ -10,3 +10,23 @@ describe('#convertCase file should exist', function() {
     expect(convertCase).to.not.be.undefined;
   });
 });
+
+// Lower case
+describe('#convert text to lower case', function() {
+  it('lower function should exist', function() {
+    expect(convertCase.lower).to.not.be.undefined;
+  });
+
+  // 'HELLO' should return 'hello'
+  it('should return a string with all lower case characters', function() {
+    expect(convertCase.lower('HELLO')).to.equal('hello');
+  });
+
+  // 'HELLO RICHARD' should return 'hello richard'
+  it('should return a string with all lower case characters', function() {
+    expect(convertCase.lower('HELLO RICHARD')).to.equal('hello richard');
+  });
+});
+
+// Upper case
+// Sentence case
