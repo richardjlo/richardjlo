2169482b715800c8a93f47c1ce6ce45a2d7cd509
Richard Lo <richardjosonlo@gmail.com>
2018-07-08 16:59:29 +0100

    added upper function

--- routes/convert-case-server.js
+++ routes/convert-case-server.js
@@ -1,3 +1,7 @@
 module.exports.lower = function(text) {
   return text.toLowerCase();
 };
+
+module.exports.upper = function(text) {
+  return text.toUpperCase();
+};
--- test/convert-case-server-spec.js
+++ test/convert-case-server-spec.js
@@ -26,7 +26,22 @@ describe('#convert text to lower case', function() {
   it('should return a string with all lower case characters', function() {
     expect(convertCase.lower('HELLO RICHARD')).to.equal('hello richard');
   });
+
+  // 'HELLO RICHARD' should return 'hello richard'
+  it('should return a string with all lower case characters', function() {
+    expect(convertCase.lower('H3LL0 R1CHARD222')).to.equal('h3ll0 r1chard222');
+  });
 });
 
+describe('#convert text to upper case', function() {
+  it('upper function should exist', function() {
+    expect(convertCase.upper).to.not.be.undefined;
+  });
+
+  // 'HELLO RICHARD' should return 'hello richard'
+  it('should return a string with all upper case characters', function() {
+    expect(convertCase.upper('hello richard')).to.equal('HELLO RICHARD');
+  });
+});
 // Upper case
 // Sentence case
