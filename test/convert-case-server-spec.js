3410add40ad4aa21e80cd638dd3524be43109a12
Richard Lo <richardjosonlo@gmail.com>
2018-07-10 17:29:11 +0100

    removed unnecessary tests

--- test/convert-case-server-spec.js
+++ test/convert-case-server-spec.js
@@ -30,46 +30,6 @@ describe('#convertCase function', function() {
   });
 });
 
-describe('#convertCase file should exist', function() {
-  it('convert-case-server.js file should exist', function() {
-    expect(convertCase).to.not.be.undefined;
-  });
-});
-
-// Lower case
-describe('#convert text to lower case', function() {
-  it('lower function should exist', function() {
-    expect(convertCase.lower).to.not.be.undefined;
-  });
-
-  // 'HELLO' should return 'hello'
-  it('should return a string with all lower case characters', function() {
-    expect(convertCase.lower('HELLO')).to.equal('hello');
-  });
-
-  // 'HELLO RICHARD' should return 'hello richard'
-  it('should return a string with all lower case characters', function() {
-    expect(convertCase.lower('HELLO RICHARD')).to.equal('hello richard');
-  });
-
-  // 'HELLO RICHARD' should return 'hello richard'
-  it('should return a string with all lower case characters', function() {
-    expect(convertCase.lower('H3LL0 R1CHARD222')).to.equal('h3ll0 r1chard222');
-  });
-});
-
-// Upper case
-describe('#convert text to upper case', function() {
-  it('upper function should exist', function() {
-    expect(convertCase.upper).to.not.be.undefined;
-  });
-
-  // 'HELLO RICHARD' should return 'hello richard'
-  it('should return a string with all upper case characters', function() {
-    expect(convertCase.upper('hello richard')).to.equal('HELLO RICHARD');
-  });
-});
-
 // Sentence case
 // Capitalize the first letter after a '.', '!', '?'
 describe('#convert to sentence case.', function() {
