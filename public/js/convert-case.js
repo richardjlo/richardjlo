d50ab233bb95d552387509c67b872d81f2f5c3fe
Richard Lo <richardjosonlo@gmail.com>
2018-07-12 18:30:02 +0100

    updated comments

--- public/js/convert-case.js
+++ public/js/convert-case.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // When any of the convert buttons are clicked, convert text.
   $('.convert-btn').click(function() {
     $.ajax({
       type: 'POST',
@@ -28,42 +29,3 @@ $(document).ready(function() {
     alert('Copied the text!');
   });
 });
-
-// let copyText = function(textArea) {
-//     /* Select the text field */
-//     textArea.select();
-//
-//     /* Copy the text inside the text field */
-//     document.execCommand('copy');
-//
-//     /* Alert the copied text */
-//     alert('Copied the text: ' + textArea.value);
-// };
-//
-// let getText = function(cb) {
-//     /* Get the text field */
-//     let textArea = document.getElementById('textArea');
-//     cb(textArea);
-// };
-
-
-// Todo: Figure out how to run copyToClipboard synchronously after ajax.
-// Needs to execute execCommand AFTER getting copyText.
-// $(document).ajaxStop( function() {
-//     copyToClipboard();
-// });
-
-
-// let copyToClipboard = function() {
-//   /* Get the text field */
-//   let copyText = document.getElementById('textArea');
-//
-//   /* Select the text field */
-//   copyText.select();
-//
-//   /* Copy the text inside the text field */
-//   document.execCommand('copy');
-//
-//   /* Alert the copied text */
-//   alert('Copied the text: ' + copyText.value);
-// };
