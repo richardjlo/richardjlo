2628dc6248ab5ffe0cd68dfd669c3ee4c93449c6
Richard Lo <richardjosonlo@gmail.com>
2018-07-13 11:56:48 +0100

    implemented clipboardjs

--- public/js/convert-case.js
+++ public/js/convert-case.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
+  // Enable copy to clipboard functionality
+  copyToClipboard();
+
   // When any of the convert buttons are clicked, convert text.
   $('.convert-btn').click(function() {
     $.ajax({
@@ -18,22 +21,20 @@ $(document).ready(function() {
       },
     });
   });
-
-  // // Copies text when 'Copy' button is clicked.
-  // $('#copy-btn').click(function() {
-  //   /* Get the text field */
-  //   let copyText = document.getElementById('textArea');
-  //
-  //   /* Select the text field */
-  //   copyText.select();
-  //
-  //   /* Copy the text inside the text field */
-  //   document.execCommand('copy');
-  //
-  //   /* Update tooltip */
-  //   let originalText = $('#copy-btn').attr('data-original-title');
-  //   $('#copy-btn').attr('data-original-title', 'Copied!').tooltip('show');
-  //   $('#copy-btn').attr('data-original-title', originalText);
-  // });
-
 });
+
+let copyToClipboard = function() {
+  let clipboard = new ClipboardJS('#copy-btn');
+
+  clipboard.on('success', function(e) {
+    // Update tooltip
+    let originalText = $('#copy-btn').attr('data-original-title');
+    $('#copy-btn').attr('data-original-title', 'Copied!').tooltip('show');
+    $('#copy-btn').attr('data-original-title', originalText);
+  });
+
+  clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+  });
+};
