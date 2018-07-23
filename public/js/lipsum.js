f3da4c80dda011dad03a4e6ee788f996d1dcbd03
Richard Lo <richardjosonlo@gmail.com>
2018-07-23 17:49:16 +0200

    added cards and js code

---
+++ public/js/lipsum.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  initJS();
+});
+
+// Initialize functions after page loads.
+let initJS = function() {
+  // Enable tooltips
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // Enable copy to clipboard functionality
+  copyToClipboard();
+};
+
+// Copy to clipboard function. Also handles tooltip.
+let copyToClipboard = function() {
+  let clipboard = new ClipboardJS('.copy-btn');
+
+  clipboard.on('success', function(e) {
+    // Update tooltip
+    // let originalText = $('#copy-btn').attr('data-original-title');
+    // $('#copy-btn').attr('data-original-title', 'Copied!').tooltip('show');
+    // $('#copy-btn').attr('data-original-title', originalText);
+  });
+
+  clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+  });
+};
