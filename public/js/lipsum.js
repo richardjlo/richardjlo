fcf953c1b23b1ec81e72dd0faab764aefaf1bf82
Richard Lo <richardjosonlo@gmail.com>
2018-07-23 22:37:00 +0200

    copy tool tips working

--- public/js/lipsum.js
+++ public/js/lipsum.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-  copyToClipboard();
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  $('.copy-btn').click(function() {
+    copyToClipboard();
+  });
 });
+
 
 // Copy to clipboard function. Also handles tooltip.
 let copyToClipboard = function() {
   let clipboard = new ClipboardJS('.copy-btn');
 
   clipboard.on('success', function(e) {
+    let btn = e.trigger;
+
+    // Update tooltip
+    let originalText = $(btn).attr('data-original-title');
+    $(btn).attr('data-original-title', 'Copied!').tooltip('show');
+    $(btn).attr('data-original-title', originalText);
   });
 
   clipboard.on('error', function(e) {
