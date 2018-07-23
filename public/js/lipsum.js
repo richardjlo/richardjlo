9a7046fd4aa3baaf63a2124cd2fa0e0ec54dd6b1
Richard Lo <richardjosonlo@gmail.com>
2018-07-23 22:49:44 +0200

    decomposed showTooltip, resetTooltip, and hideTooltip

--- public/js/lipsum.js
+++ public/js/lipsum.js
@@ -15,15 +15,29 @@ let copyToClipboard = function() {
 
   clipboard.on('success', function(e) {
     let btn = e.trigger;
+    let originalMessage = $(btn).attr('data-original-title');
 
-    // Update tooltip
-    let originalText = $(btn).attr('data-original-title');
-    $(btn).attr('data-original-title', 'Copied!').tooltip('show');
-    $(btn).attr('data-original-title', originalText);
+    showTooltip(btn, 'Copied!');
+    hideTooltip(btn);
+    resetTooltip(btn, originalMessage);
   });
 
   clipboard.on('error', function(e) {
       console.error('Action:', e.action);
       console.error('Trigger:', e.trigger);
   });
+};
+
+let showTooltip = function(btn, message) {
+  $(btn).attr('data-original-title', message).tooltip('show');
+};
+
+let resetTooltip = function(btn, originalMessage) {
+  $(btn).attr('data-original-title', originalMessage);
+};
+
+let hideTooltip = function(btn) {
+  setTimeout(function() {
+    $(btn).tooltip('hide');
+  }, 1000);
 };
