74839e70d8e6fa6f00d8679728a00a7ad3fb85aa
Richard Lo <richardjosonlo@gmail.com>
2018-07-30 00:27:52 +0200

    on page refresh, takes  all transactions in db and adds them to table on UI

--- public/js/expense-tracker.js
+++ public/js/expense-tracker.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
-  // let transactionsRef = db.ref('transactions/' + transactionId);
-  // transactionsRef.on('value', function(snapshot) {
-  //   let transactions = snapshot.val();
-  //   console.log(transactions[1]);
-  //   console.log(transactions[1].amount);
-  //   console.log(transactions[1].description);
-  //   console.log(transactions[1].vendor);
-  // });
+  addTransactionsToScreen();
 
   $('#addExpense').click(function() {
     let description = $('#description').val();
     let vendor = $('#vendor').val();
     let amount = $('#amount').val();
     createTransaction(description, vendor, amount);
-    // alert('button clicked!');
+    // addTransactionToScreen(description, vendor, amount);
   });
 });
 
@@ -24,4 +17,26 @@ let createTransaction = function(description, vendor, amount) {
       vendor: vendor,
       amount: amount,
   });
+};
+
+let addTransactionsToScreen = function() {
+  let transactionsRef = db.ref('transactions/').orderByKey();
+  transactionsRef.once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      let childKey = childSnapshot.key;
+      let childData = childSnapshot.val();
+      addTransactionToScreen(childData.description, childData.vendor,
+        childData.amount);
+    });
+  });
+};
+
+let addTransactionToScreen = function(description, vendor, amount) {
+  $('#transactionsTable').append(
+    '<tr><td>'+
+      description + '</td><td>' +
+      vendor + '</td><td>$' +
+      amount +
+    '</td></tr>'
+  );
 };
