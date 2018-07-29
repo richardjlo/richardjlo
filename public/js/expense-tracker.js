6bc05a7aa3a8e5f2149204f4d045f8ee5c1b40db
Richard Lo <richardjosonlo@gmail.com>
2018-07-29 23:43:47 +0200

    connected form to db to create new transactions

--- public/js/expense-tracker.js
+++ public/js/expense-tracker.js
@@ -1,18 +1,27 @@
-// Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyCDt4GDmJ7PBUh7AIhRdnD-kD5tleAzciQ',
-  authDomain: 'expense-tracker-a3686.firebaseapp.com',
-  databaseURL: 'https://expense-tracker-a3686.firebaseio.com',
-  projectId: 'expense-tracker-a3686',
-  storageBucket: 'expense-tracker-a3686.appspot.com',
-  messagingSenderId: '487206506379',
-};
-firebase.initializeApp(config);
+$(document).ready(function() {
+  // let transactionsRef = db.ref('transactions/' + transactionId);
+  // transactionsRef.on('value', function(snapshot) {
+  //   let transactions = snapshot.val();
+  //   console.log(transactions[1]);
+  //   console.log(transactions[1].amount);
+  //   console.log(transactions[1].description);
+  //   console.log(transactions[1].vendor);
+  // });
 
-let transactionId = 4;
-let db = firebase.database();
-db.ref('transactions/' + transactionId).set({
-    descripiton: 'Dinner for 5',
-    vendor: 'Can Bey Doner Kebab',
-    amount: '$9.08',
+  $('#addExpense').click(function() {
+    let description = $('#description').val();
+    let vendor = $('#vendor').val();
+    let amount = $('#amount').val();
+    createTransaction(description, vendor, amount);
+    // alert('button clicked!');
+  });
 });
+
+let createTransaction = function(description, vendor, amount) {
+  let newTransaction = db.ref('transactions/').push();
+  newTransaction.set({
+      description: description,
+      vendor: vendor,
+      amount: amount,
+  });
+};
