98bcdc316a0c4a8f5afa08c8b9363dbba53b078d
Richard Lo <richardjosonlo@gmail.com>
2018-07-31 19:06:20 +0200

    decomposed renderTransactions

--- public/js/expense-tracker.js
+++ public/js/expense-tracker.js
@@ -1,6 +1,110 @@
 $(document).ready(function() {
   const db = firebase.database();
   let transactionsRef = db.ref('transactions/');
+
+  // Updates screen with latest transactions (new, update, delete)
+  renderTransactions(transactionsRef);
+
+  // When the new transaction form is submitted, create new transaction in db
+  let newExpenseform = $('#newExpenseForm');
+  $(newExpenseform).submit(function(e) {
+    // Stop browser from submitting form
+    e.preventDefault();
+
+    let description = $('#description').val();
+    let vendor = $('#vendor').val();
+    let amount = $('#amount').val();
+    createTransaction(transactionsRef, description, vendor, amount);
+    $(newExpenseform).trigger('reset');
+    $('#newExpenseModal').modal('toggle');
+  });
+
+  // When edit button is clicked, update transaction in db
+  $('#transactionsTable').on('click', '.edit-btn', function() {
+    $('#updateExpenseModal').modal('toggle');
+    /*
+    // TO DO - prepoulate form with current transaction values.
+    let amount = $(this).parent().siblings('.amount').text();
+    let description = $(this).parent().siblings('.description').text();
+    let vendor = $(this).parent().siblings('.vendor').text();
+    */
+
+    let key = $(this).parent().parent().attr('id');
+    let transaction = db.ref('transactions/' + key);
+
+    // When the transaction form is submitted, update transaction in db
+    let updateExpenseForm = $('#updateExpenseForm');
+    $(updateExpenseForm).submit(function(et) {
+      // Stop browser from submitting form
+      et.preventDefault();
+
+      let newDescription = $('#update-description').val();
+      let newVendor = $('#update-vendor').val();
+      let newAmount = $('#update-amount').val();
+
+      console.log(newDescription + ' ' + newVendor + ' ' + newAmount);
+      transaction.update({
+        amount: newAmount,
+        description: newDescription,
+        vendor: newVendor,
+      });
+      // $(updateExpenseForm).reset();
+      // $('#updateExpenseModal').modal('toggle');
+    });
+  });
+
+  // When delete button is clicked, delete transaction from db
+  $('#transactionsTable').on('click', '.delete-btn', function() {
+    let key = $(this).parent().parent().attr('id');
+    let transaction = db.ref('transactions/' + key);
+    transaction.remove();
+  });
+});
+
+// Create new transaction in db
+let createTransaction = function(transactionsRef, description, vendor, amount) {
+  let newTransaction = transactionsRef.push();
+  newTransaction.set({
+      description: description,
+      vendor: vendor,
+      amount: amount,
+  });
+};
+
+// Add transaction element to screen
+let addTransactionElement = function(transactionsTable, key, description,
+  vendor, amount) {
+  transactionsTable.append(
+    '<tr id=' + key + '>' +
+      '<td class="description">' + description + '</td>' +
+      '<td class="vendor">' + vendor + '</td>' +
+      '<td class="amount">$' + amount + '</td>' +
+      '<td><button type="button" class="btn btn-sm btn-outline-secondary edit-btn">Edit</button></td>' +
+      '<td><button type="button" class="btn btn-sm btn-outline-danger delete-btn">Delete</button></td>' +
+    '</tr>'
+  );
+};
+
+// Update transaction element on screen
+let setTransactionValues = function(transactionElement, key, description,
+  vendor, amount) {
+  transactionElement.replaceWith(
+    '<tr id=' + key + '>' +
+      '<td class="description">' + description + '</td>' +
+      '<td class="vendor">' + vendor + '</td>' +
+      '<td class="amount">$' + amount + '</td>' +
+      '<td><button type="button" class="btn btn-sm btn-outline-secondary edit-btn">Edit</button></td>' +
+      '<td><button type="button" class="btn btn-sm btn-outline-danger delete-btn">Delete</button></td>' +
+    '</tr>'
+  );
+};
+
+// Remove transaction element from screen
+let removeTransaction = function(transactionElement) {
+  transactionElement.remove();
+};
+
+let renderTransactions = function(transactionsRef) {
   let key;
   let transaction;
   let transactionElement;
@@ -29,73 +133,4 @@ $(document).ready(function() {
     transactionElement = $('#' + key);
     removeTransaction(transactionElement);
   });
-
-  // When the new transaction form is submitted, create new transaction in db
-  let form = $('#newExpenseForm');
-  $(form).submit(function(e) {
-    // Stop browser from submitting form
-    e.preventDefault();
-
-    let description = $('#description').val();
-    let vendor = $('#vendor').val();
-    let amount = $('#amount').val();
-    createTransaction(transactionsRef, description, vendor, amount);
-    $(form).trigger('reset');
-    $('#newExpenseModal').modal('toggle');
-  });
-
-  // When edit button is clicked, update transaction in db
-  $('#transactionsTable').on('click', '.edit-btn', function() {
-    alert('transaction edited!');
-  });
-
-  // When delete button is clicked, delete transaction from db
-  $('#transactionsTable').on('click', '.delete-btn', function() {
-    let key = $(this).parent().parent().attr('id');
-    let transaction = db.ref('transactions/' + key);
-    transaction.remove();
-  });
-});
-
-// Create new transaction in db
-let createTransaction = function(transactionsRef, description, vendor, amount) {
-  let newTransaction = transactionsRef.push();
-  newTransaction.set({
-      description: description,
-      vendor: vendor,
-      amount: amount,
-  });
-};
-
-// Create transaction
-let addTransactionElement = function(transactionsTable, key, description,
-  vendor, amount) {
-  transactionsTable.append(
-    '<tr id=' + key + '>' +
-      '<td>' + description + '</td>' +
-      '<td>' + vendor + '</td>' +
-      '<td>$' + amount + '</td>' +
-      '<td><button type="button" class="btn btn-sm btn-outline-secondary edit-btn">Edit</button></td>' +
-      '<td><button type="button" class="btn btn-sm btn-outline-danger delete-btn">Delete</button></td>' +
-    '</tr>'
-  );
-};
-
-// Set transaction
-let setTransactionValues = function(transactionElement, key, description,
-  vendor, amount) {
-  transactionElement.replaceWith(
-    '<tr id=' + key + '>' +
-      '<td>' + description + '</td>' +
-      '<td>' + vendor + '</td>' +
-      '<td>$' + amount + '</td>' +
-      '<td><button type="button" class="btn btn-sm btn-outline-secondary edit-btn">Edit</button></td>' +
-      '<td><button type="button" class="btn btn-sm btn-outline-danger delete-btn">Delete</button></td>' +
-    '</tr>'
-  );
-};
-
-// Delete transaction
-let removeTransaction = function(transactionElement) {
-  transactionElement.remove();
 };
