ee24bf230f052a9c751c6fe148a3599498f992b3
Richard Lo <richardjosonlo@gmail.com>
2018-08-10 15:01:46 +0200

    added comment

--- routes/weather-server.js
+++ routes/weather-server.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const open_weather_api_key = process.env.OPEN_WEATHER_API_KEY;
 
+
+// TODO - Return weatherStr AFTER request function is complete.
 module.exports.getWeather = (city) => {
   let weatherStr = '';
   let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + open_weather_api_key;
