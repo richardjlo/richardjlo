efb8f8c6b6a29c013116aa317db9aba438fa3273
Richard Lo <richardjosonlo@gmail.com>
2018-08-10 16:42:32 +0200

    added callback code to handle async functions

--- public/js/callbacks.js
+++ public/js/callbacks.js
@@ -1,3 +1,65 @@
+// Broken code. Want createPost to execute first THEN getPosts. Want 3 posts to appear on screen, but only 2 do.
+/*
 $(document).ready(function() {
-  alert('hi');
+  createPost({title: 'Post Three', body: 'This is post three'});
+  getPosts();
 });
+
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'}
+];
+
+// Returns result after 2 seconds
+getPosts = function() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li> ${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+};
+
+// Returns result after 1 second
+createPost = function(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+};
+*/
+
+
+
+
+// Using Callbacks
+/*
+$(document).ready(function() {
+  createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+});
+
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'}
+];
+
+// Returns result after 1 seconds
+getPosts = function() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li> ${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+};
+
+// Returns result after 2 second
+createPost = function(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+};
+
+*/
