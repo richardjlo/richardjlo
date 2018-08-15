e445d885221e2f42d24995ec1181b5d6ca0b06e7
Richard Lo <richardjosonlo@gmail.com>
2018-08-15 14:35:36 +0200

    renamed api key const and removed request require statement

--- routes/weather-server.js
+++ routes/weather-server.js
@@ -1,9 +1,9 @@
-const request = require('request');
 const rp = require('request-promise');
-const open_weather_api_key = process.env.OPEN_WEATHER_API_KEY;
+const openWeatherApiKey = process.env.OPEN_WEATHER_API_KEY;
 
 module.exports.getWeather = (city) => {
-  let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + open_weather_api_key;
+  let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +
+    '&units=imperial&appid=' + openWeatherApiKey;
   rp(url, (error, response, body) => {
   }).then(function(body) {
     let result = JSON.parse(body);
