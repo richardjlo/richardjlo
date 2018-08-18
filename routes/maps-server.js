6634881e1078712548431e876988467ae73f878e
Richard Lo <richardjosonlo@gmail.com>
2018-08-18 21:03:32 +0200

    hello world foursquare api. searching for vegan food

--- routes/maps-server.js
+++ routes/maps-server.js
@@ -1,3 +1,4 @@
+/*
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
 module.exports.helloWorld = () => {
@@ -26,4 +27,39 @@ module.exports.helloWorld = () => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+*/
+
+module.exports.helloWorld = () => {
+  const request = require('request');
+  let fourSquareSecret = process.env.FOURSQUARE_CLIENT_SECRET;
+
+  request({
+    url: 'https://api.foursquare.com/v2/venues/search',
+    method: 'GET',
+    qs: {
+      client_id: 'Y0IWGKGNDLQUEOA2QLRUILZYN5DNGMTMXAIJZAB00YLQZETR',
+      client_secret: fourSquareSecret,
+      radius: '1000',
+      section: 'food',
+      categoryId: '4bf58dd8d48988d1d3941735',
+      ll: '52.510411, 13.457715',
+      v: '20180323',
+      limit: 5,
+    },
+  }, function(err, res, body) {
+    if (err) {
+      console.error(err);
+    } else {
+      let result = JSON.parse(body);
+      let venues = result.response.venues;
+      let count = 1;
+      for (let venue of venues) {
+        // console.log('ID: ' + venue.id + ' Name: ' + venue.name);
+        console.log(count + ': ' + venue.name);
+        count++;
+      }
+    }
+  });
 };
