6df0c9cb4b5a3bd0763549ac61fcc0a22cf2221b
Richard Lo <richardjosonlo@gmail.com>
2018-08-18 17:38:13 +0200

    searching for vegan living restaurant and return detailed place results

--- routes/maps-server.js
+++ routes/maps-server.js
@@ -1,5 +1,29 @@
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
 module.exports.helloWorld = () => {
-  console.log('hello world!');
+  const googleMapsClient = require('@google/maps').createClient({
+    key: googleMapsApiKey,
+    Promise: Promise,
+  });
+
+  googleMapsClient.places({query: '1990 vegan living'})
+    .asPromise()
+    .then((response) => {
+      let place = response.json.results;
+      // console.log(place);
+      let placeId = place[0].place_id;
+      googleMapsClient.place({
+          placeid: placeId,
+          fields: ['name', 'opening_hours', 'price_level', 'rating'],
+        })
+        .asPromise()
+        .then((response) => {
+          console.log(response.json.result);
+        }).catch((err) => {
+          console.log('Error: ' + err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
