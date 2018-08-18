5142f638ce30c03f12fa85fdbbb99dcdd0280699
Richard Lo <richardjosonlo@gmail.com>
2018-08-18 21:13:56 +0200

    added request-promise

--- routes/maps-server.js
+++ routes/maps-server.js
@@ -1,3 +1,4 @@
+
 /*
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
@@ -31,6 +32,7 @@ module.exports.helloWorld = () => {
 
 */
 
+/*
 module.exports.helloWorld = () => {
   const request = require('request');
   let fourSquareSecret = process.env.FOURSQUARE_CLIENT_SECRET;
@@ -61,5 +63,46 @@ module.exports.helloWorld = () => {
         count++;
       }
     }
+  });
+};
+
+*/
+const rp = require('request-promise');
+let fourSquareSecret = process.env.FOURSQUARE_CLIENT_SECRET;
+
+module.exports.helloWorld = () => {
+  // const googleMapsClient = require('@google/maps').createClient({
+  //   key: googleMapsApiKey,
+  //   Promise: Promise,
+  // });
+
+  rp({
+    url: 'https://api.foursquare.com/v2/venues/search',
+    method: 'GET',
+    qs: {
+      client_id: 'Y0IWGKGNDLQUEOA2QLRUILZYN5DNGMTMXAIJZAB00YLQZETR',
+      client_secret: fourSquareSecret,
+      radius: '1000',
+      section: 'food',
+      categoryId: '4bf58dd8d48988d1d3941735',
+      ll: '52.510411, 13.457715',
+      v: '20180323',
+      limit: 5,
+    },
+  }, function(err, response, body) {
+    if (err) {
+      console.error(err);
+    } else {
+      let result = JSON.parse(body);
+      let venues = result.response.venues;
+      let count = 1;
+      for (let venue of venues) {
+        // console.log('ID: ' + venue.id + ' Name: ' + venue.name);
+        console.log(count + ': ' + venue.name);
+        count++;
+      }
+    }
+  }).then(() => {
+    console.log('all done.');
   });
 };
