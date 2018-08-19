49ac0e491a3d000be8cff0466d76cdb6ecacca4d
Richard Lo <richardjosonlo@gmail.com>
2018-08-19 02:22:37 +0200

    search for vegan restaurants nearby and print venue details (name and rating

--- routes/maps-server.js
+++ routes/maps-server.js
@@ -36,43 +36,38 @@ const rp = require('request-promise');
 let fourSquareSecret = process.env.FOURSQUARE_CLIENT_SECRET;
 
 module.exports.helloWorld = () => {
-  // const googleMapsClient = require('@google/maps').createClient({
-  //   key: googleMapsApiKey,
-  //   Promise: Promise,
-  // });
+  // Get vegan restaurants in area.
+  rp({
+    url: 'https://api.foursquare.com/v2/venues/search',
+    method: 'GET',
+    qs: {
+      client_id: 'Y0IWGKGNDLQUEOA2QLRUILZYN5DNGMTMXAIJZAB00YLQZETR',
+      client_secret: fourSquareSecret,
+      radius: '1000',
+      section: 'food',
+      categoryId: '4bf58dd8d48988d1d3941735',
+      ll: '52.510411, 13.457715',       // TODO - update with user's lat, long
+      v: '20180323',
+      limit: 5,
+    },
+  }, function(err, response, body) {
+    if (err) {
+      console.error(err);
+    } else {
+      let result = JSON.parse(body);
+      let venues = result.response.venues;
+      for (let venue of venues) {
+        getVenue(venue.id);
+      }
+    }
+  }).then(() => {
+    // console.log('all done.');
+  });
+};
 
-  // rp({
-  //   url: 'https://api.foursquare.com/v2/venues/search',
-  //   method: 'GET',
-  //   qs: {
-  //     client_id: 'Y0IWGKGNDLQUEOA2QLRUILZYN5DNGMTMXAIJZAB00YLQZETR',
-  //     client_secret: fourSquareSecret,
-  //     radius: '1000',
-  //     section: 'food',
-  //     categoryId: '4bf58dd8d48988d1d3941735',
-  //     ll: '52.510411, 13.457715',
-  //     v: '20180323',
-  //     limit: 5,
-  //   },
-  // }, function(err, response, body) {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //     let result = JSON.parse(body);
-  //     let venues = result.response.venues;
-  //     let count = 1;
-  //     for (let venue of venues) {
-  //       // console.log('ID: ' + venue.id + ' Name: ' + venue.name);
-  //       console.log(count + ': ' + venue.name);
-  //       count++;
-  //     }
-  //   }
-  // }).then(() => {
-  //   console.log('all done.');
-  // });
 
-  // Get name, rating, and price tier of restaurant.
-  let venueId = '58669dfd13af1c32d09c0a44'; // 1990 Vegan Living
+// Get name, rating of restaurants.
+let getVenue = (venueId) => {
   rp({
     url: 'https://api.foursquare.com/v2/venues/' + venueId,
     method: 'GET',
@@ -86,11 +81,62 @@ module.exports.helloWorld = () => {
       console.error(err);
     } else {
       let venue = JSON.parse(body).response.venue;
-      console.log('Name: ' + venue.name);
-      console.log('Rating: ' + venue.rating);
-      console.log('Price tier: ' + venue.price.tier);
+      console.log(venue.name + ' ' + venue.rating);
     }
-  }).then(() => {
-    console.log('all done.');
   });
 };
+
+
+// Get vegan restaurants in area.
+// rp({
+//   url: 'https://api.foursquare.com/v2/venues/search',
+//   method: 'GET',
+//   qs: {
+//     client_id: 'Y0IWGKGNDLQUEOA2QLRUILZYN5DNGMTMXAIJZAB00YLQZETR',
+//     client_secret: fourSquareSecret,
+//     radius: '1000',
+//     section: 'food',
+//     categoryId: '4bf58dd8d48988d1d3941735',
+//     ll: '52.510411, 13.457715',
+//     v: '20180323',
+//     limit: 5,
+//   },
+// }, function(err, response, body) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     let result = JSON.parse(body);
+//     let venues = result.response.venues;
+//     let count = 1;
+//     for (let venue of venues) {
+//       // console.log('ID: ' + venue.id + ' Name: ' + venue.name);
+//       console.log(count + ': ' + venue.name);
+//       count++;
+//     }
+//   }
+// }).then(() => {
+//   console.log('all done.');
+// });
+
+// Get name, rating, and price tier of restaurant.
+// let venueId = '58669dfd13af1c32d09c0a44'; // 1990 Vegan Living
+// rp({
+//   url: 'https://api.foursquare.com/v2/venues/' + venueId,
+//   method: 'GET',
+//   qs: {
+//     client_id: 'Y0IWGKGNDLQUEOA2QLRUILZYN5DNGMTMXAIJZAB00YLQZETR',
+//     client_secret: fourSquareSecret,
+//     v: '20180323',
+//   },
+// }, function(err, response, body) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     let venue = JSON.parse(body).response.venue;
+//     console.log('Name: ' + venue.name);
+//     console.log('Rating: ' + venue.rating);
+//     console.log('Price tier: ' + venue.price.tier);
+//   }
+// }).then(() => {
+//   console.log('all done.');
+// });
