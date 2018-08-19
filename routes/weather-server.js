ccebdf695228cbfbcbb69187f9e7cbb60e4821ce
Richard Lo <richardjosonlo@gmail.com>
2018-08-19 10:37:28 +0200

    moved logic to then function

--- routes/weather-server.js
+++ routes/weather-server.js
@@ -5,13 +5,13 @@ module.exports.getWeather = (city, res, next) => {
   let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +
     '&units=imperial&appid=' + openWeatherApiKey;
   rp(url, (error, response, body) => {
+  })
+  .then((body) => {
     let result = JSON.parse(body);
+    console.log(result);
     let temp = result.main.temp;
     let weatherStr = 'It\'s currently '+ temp + '℉ in ' + city;
     res.send(weatherStr);
-  })
-  .then((body) => {
-    // console.log('All done');
   })
   .catch(function(error) {
     next(error);
